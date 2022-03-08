1b43950a80a2623f1184c122ae593ff1d639510a
nicolas-bofi <nicolas.bofi@etu.umontpellier.fr>
2022-03-08 22:46:36 +0100

    loading more

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,15 +5,10 @@ import '../styles/index.css';
 
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts]: [any, any] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [categories, setCategories]: [string[], any] = useState([])
-
-
-  function handleClick(e){
-    e.preventDefault();
-    console.log("coucou")
-  }
+  const [visible, setVisible] = useState(3)
 
   useEffect(() => {
     axios.get("/api/posts").then(function (response) {
@@ -39,21 +34,63 @@ function App() {
       // always executed
     });
   }, [])
-  
+
+  function loadMore() {
+    setVisible(visible + 3)
+  }
+
+  function isSelectable(post){
+    var toReturn = false
+    post.categories.forEach(category => {
+      if(category.name === selectedCategory){
+        toReturn =  true
+      }
+    })
+    return toReturn
+  }
+
+  function all(){
+    return (
+      <div>
+        {
+          posts.slice(0, visible).map(function(post: any){
+            return <Post key={post.id} {...{post: post}}/>
+          })
+        }
+        {
+          visible < posts.length &&
+            <div onClick={loadMore} className="load-more">Load more</div>
+        }
+      </div>
+    )
+  }
+
+  function filtered(){
+    return (
+      <div>
+        {
+          posts.filter((post) => isSelectable(post)).slice(0, visible).map(function(p: any){
+            console.log(p.title)
+            return <Post key={p.id} {...{post: p}}/>
+          })
+        }
+        {
+          visible < posts.filter((post) => isSelectable(post)).length &&
+            <div onClick={loadMore} className="load-more">Load more</div>
+        }
+      </div>
+    )
+  }
 
   return (
     <div id="list">
       <h1>Posts</h1>
       <div id='categorieList' >
         {categories.map((category:string) => 
-          <button key={category.toString()} onClick={() => setSelectedCategory(category)} className={category == selectedCategory ? "selected" : "notSelected"}>{category}</button>
+          <div key={category.toString()} onClick={() => {setSelectedCategory(category); setVisible(3)}} className={category === selectedCategory ? "selected" : "notSelected"}>{category}</div>
         )}
       </div>
-        {posts.map(function(post: any){
-          if(selectedCategory == "All" || post.categories.some(c => c.name === selectedCategory)){
-            return <Post {...{post: post}}/>
-          }
-        })}
+      {selectedCategory === "All" ? all() : filtered()}
     </div>
   );
 }
